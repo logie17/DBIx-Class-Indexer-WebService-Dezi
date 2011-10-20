fc404aa665ef3e0a6fcb0af38a98a81704f4ec6d
Logan <loganbell@gmail.com>
2011-10-19 17:04:55 -0700

    Initial commit

---
+++ t/sql/sqlite.sql
@@ -0,0 +1,9 @@
+create table person (
+    person_id       integer primary key not null,
+    name            varchar(128) not null,
+    age             integer not null,
+    email           varchar(128),
+    image_path      varchar(128),
+    created         timestamp not null    
+);
+
