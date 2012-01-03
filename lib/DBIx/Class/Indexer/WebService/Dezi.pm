6999f95c28c9eec030c858e7b640e55b49e5d82c
Logan <loganbell@gmail.com>
2012-01-02 23:24:16 -0800

    Upped' version and changes file.

--- lib/DBIx/Class/Indexer/WebService/Dezi.pm
+++ lib/DBIx/Class/Indexer/WebService/Dezi.pm
@@ -16,11 +16,11 @@ DBIx::Class::Indexer::WebService::Dezi - An indexer for Dezi/Lucy.
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 around BUILDARGS => sub {
     my ( $orig, $class, $connect_info, $source ) = @_;
